557115276d837815277c46d6af109fc2882bc01d
Abhinav Gautam <abhinav.nanhey@gmail.com>
2023-07-13 01:03:58 +0530

    try catch in cons

--- utils/upload/gdrive.js
+++ utils/upload/gdrive.js
@@ -1,55 +1,61 @@
-const drive = require('@googleapis/drive')
-const fs = require('fs');
+const drive = require("@googleapis/drive");
+const fs = require("fs");
 class GDrive {
-    
-    constructor(options) {
-        let {serviceAccountKey,serviceAccountKeyPath} = options;
-        if(serviceAccountKey && !fs.existsSync(serviceAccountKeyPath)) {
-            fs.writeFileSync(path.join(serviceAccountKeyPath), serviceAccountKey, 'utf8');
-        }
-        
-        let auth = new drive.auth.GoogleAuth({
-            keyFilename: serviceAccountKeyPath,
-            scopes:['https://www.googleapis.com/auth/drive']
-        });
+  constructor(options) {
+    let { serviceAccountKey, serviceAccountKeyPath } = options;
+    try {
+      if (serviceAccountKey && !fs.existsSync(serviceAccountKeyPath)) {
+        fs.writeFileSync(
+          path.join(serviceAccountKeyPath),
+          serviceAccountKey,
+          "utf8"
+        );
+      }
+      let auth = new drive.auth.GoogleAuth({
+        keyFilename: serviceAccountKeyPath,
+        scopes: ["https://www.googleapis.com/auth/drive"],
+      });
 
-        this.drive = drive.drive({
-            version: 'v3',
-            auth
-        })    
+      this.drive = drive.drive({
+        version: "v3",
+        auth,
+      });
+    } catch (e) {
+      console.log(e);
     }
+  }
 
-    async uploadFile(options) {
-        let {fileName,mimeType,mediaStream} = options;
-        let data = await this.drive.files.create({
-            requestBody: {
-              name: fileName,
-              mimeType: mimeType
-            },
-            media: {
-              mimeType: mimeType,
-              body: mediaStream
-            }
-        });
+  async uploadFile(options) {
+    let { fileName, mimeType, mediaStream } = options;
+    let data = await this.drive.files.create({
+      requestBody: {
+        name: fileName,
+        mimeType: mimeType,
+      },
+      media: {
+        mimeType: mimeType,
+        body: mediaStream,
+      },
+    });
 
-        return data;
-    }
+    return data;
+  }
 
-    async addPermissions(options) {
-        let {role,type,fileId} = options;
-        await this.drive.permissions.create({
-            fileId,
-            requestBody: {role,type}
-        });
-    }
+  async addPermissions(options) {
+    let { role, type, fileId } = options;
+    await this.drive.permissions.create({
+      fileId,
+      requestBody: { role, type },
+    });
+  }
 
-    async get(options) {
-        let {fileId} = options;
-        let file = await this.drive.files.get({fileId});
-        return file;
-    }  
+  async get(options) {
+    let { fileId } = options;
+    let file = await this.drive.files.get({ fileId });
+    return file;
+  }
 }
 
 module.exports = {
-    GDrive
+  GDrive,
 };
